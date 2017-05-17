a8816117b05896c00f44cb95e7b104b7242d0418
Aldi Unanto <aldiunanto@yahoo.com>
2017-05-17 11:55:19 +0700

    select,joi,etc

--- system/connection.php
+++ system/connection.php
@@ -13,6 +13,9 @@ $DB = new DB($conn);
 class DB{
 
 	static $_db;
+	static $select = '*';
+	static $where;
+	static $join;
 
 	public function __construct($conn){
 		self::$_db = $conn;
@@ -26,6 +29,31 @@ class DB{
 
 		$q = self::$_db->prepare('INSERT INTO '. $tbl . '(' . implode(',', array_keys($args)) . ') VALUES(' . substr($mark, 0, (strlen($mark) - 1)) . ')');
 		return $q->execute($vals);
+	}
+	public function select($who = ''){
+		if($who != '') return self::$select = $who;
+		else return self::$select;
+	}
+	public function where($field, $val = ''){
+		$w = '';
+		if(is_array($field)){
+			foreach ($field as $key => $val){
+				$w .= $key . '="' . $val . '" AND ';
+			}
+		}else{
+			$w = $field . '="' . $val . '" AND ';
+		}
+		
+		return self::$where .= substr($w, 0, (strlen($w) - 5));
+	}
+	public function join($table, $on, $inner){
+		$i  = ' ' . strtoupper($inner) . ' JOIN ';
+		$i .= $table . ' ON(' . $on . ') ';
+		
+		return self::$join .= $i;
+	}
+	public function get($tbl){
+		return self::$_db->query('SELECT ' . self::$select . ' FROM ' . $tbl . self::$join . (self::$where == '' ? null : ' WHERE ' . self::$where));
 	}
 
 }
